1834c4e051ffc37d47e82d387e7de4ce860ea010
artoorcheck <132397391+artoorcheck@users.noreply.github.com>
2024-09-21 05:37:01 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Arthur on 21.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
